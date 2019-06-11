7f0d8d3b7c1c1edd3c92b4bc0deb8c59742d1102
Admin <admin@Admins-MacBook-Pro.local>
2019-06-11 12:22:06 -0700

    adding Notes

--- src/app/hero-list.component.ts
+++ src/app/hero-list.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit }   from '@angular/core';
-
 import { Hero }                from './hero';
 import { HeroService }         from './hero.service';
 
+
+
+// This designates this a component and includes some metadata
 @Component({
-  selector:    'app-hero-list',
-  templateUrl: './hero-list.component.html',
-  providers:  [ HeroService ]
+  selector:    'app-hero-list',                 //  This assignes that the components view should be displayed whenever tag "app-hero-list" is found in the html template
+  templateUrl: './hero-list.component.html',    // This declares to which HTML template this component should attach
+  providers:  [ HeroService ]                   //
 })
+
+
+// We define a component's application logic (what it does to support the view) inside a class. 
+// The class interacts with the view through an API of properties and methods
+
 export class HeroListComponent implements OnInit {
-  heroes: Hero[];
+  heroes: Hero[];                                         // array of heroes
   selectedHero: Hero;
 
   constructor(private service: HeroService) { }
 
+
   ngOnInit() {
-    this.heroes = this.service.getHeroes();
+    this.heroes = this.service.getHeroes();  //
   }
 
-  selectHero(hero: Hero) { this.selectedHero = hero; }
+
+  selectHero(hero: Hero) { this.selectedHero = hero; }  //The selectHero() method sets a selectedHero property when the user clicks to choose a hero from that list.
 }
