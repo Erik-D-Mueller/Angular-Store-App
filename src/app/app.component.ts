244831c64b910e7e45eb37b0223d21dba006671c
Admin <admin@Admins-MacBook-Pro.local>
2019-06-11 18:05:10 -0700

    restarted fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-
-// This is the root component called "App.Component".
-// This component represents the entire application visually, all other componenets are a child of this.
-// It does not appear to have a connected class-logic piece the way that sub-components do, perhaps it's not required.
-// The .ts file extension is for typescript
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-hero-list></app-hero-list>
-    <app-sales-tax></app-sales-tax>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent { }
+export class AppComponent  {}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-//This is the root module, this module represents the entire application.
-// Pretty much every single thing in the entire application is a child of this
-
-import { BrowserModule }       from '@angular/platform-browser';
-import { FormsModule }         from '@angular/forms';
-import { NgModule }     from '@angular/core';
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroListComponent }   from './hero-list.component';
-import { SalesTaxComponent }   from './sales-tax.component';
-import { HeroService }         from './hero.service';
-import { BackendService }      from './backend.service';
-import { Logger }              from './logger.service';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent,
-    HeroListComponent,
-    SalesTaxComponent
-  ],
-  providers: [
-    BackendService,
-    HeroService,
-    Logger
+    TopBarComponent,
+    ProductListComponent
   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
